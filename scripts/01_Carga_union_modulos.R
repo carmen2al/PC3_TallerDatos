407beb6673d74cfdbac43c895af0502b664c0e45
carmen2al <carmen.andonayre@pucp.edu.pe>
2026-06-18 22:11:54 -0500

    Importar módulos de Acceso a programas sociales (37), Empleo e Ingresos (5) y Características de los miembros del hogar (2)

--- scripts/01_Carga_union_modulos.R
+++ scripts/01_Carga_union_modulos.R
@@ -5,8 +5,13 @@
 #Fecha: 18/06/26
 #===============================================================================
 
-#Cargar librerías --------------------------------------------------------------
+#Cargar librerías---------------------------------------------------------------
 library(haven)
 library(dplyr)
 renv::snapshot()
+
+#Importar datos-----------------------------------------------------------------
+mod700B <- read.csv("datos/crudos/ENAHO01-2025-700B.csv", fileEncoding = "Latin1") #Acceso a programas sociales
+mod500  <- read.csv("datos/crudos/ENAHO01A-2025-500.csv", fileEncoding = "Latin1", sep = ";") #Empleo e Ingresos
+mod200  <- read.csv("datos/crudos/ENAHO01-2025-200.csv", fileEncoding = "Latin1") #Características de los miembros del hogar
 
