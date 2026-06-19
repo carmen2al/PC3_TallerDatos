ec4aa81ca304bd4cfe4d8bdc0c562c6f2e29b8e8
carmen2al <carmen.andonayre@pucp.edu.pe>
2026-06-18 20:20:52 -0500

    Agregar script de creación de carpetas a la carpeta de "scripts"

---
+++ scripts/S_Inicial_CreacionCarpetas_180626.R
@@ -0,0 +1,16 @@
+#===============================================================================
+#Proyecto:  Taller de Procesamiento de datos- Práctica Calificada 3 
+#Autora: Carmen Andonayre
+#Objetivo: Crear un repositorio público en GitHub a través de RStudio con una estructura 
+#de gestión de datos de un proyecto de investigación empleando datos de la ENAHO 
+#Fecha: 18/06/26
+#===============================================================================
+
+#Creación de las carpetas ------------------------------------------------------
+dir.create("datos")
+dir.create("datos/crudos")
+dir.create("datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+dir.create("scripts")
+
